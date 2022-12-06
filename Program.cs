c23ba947961f9e89e4d9e825e158904b536565f0
Vlad Oreshko <orex3896@gmail.com>
2022-12-06 23:32:36 +0300

    Created task #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int DifOst(int num)
+{
+    int delEndNum = num/10;
+    int delFirstNum = delEndNum%10;
+    int result= delFirstNum;
+    return(result);
+} 
+Console.WriteLine("Введите трехзначное число ");
+int a = Convert.ToInt32( Console.ReadLine() );
+//int a = new Random().Next(100,999) ;
+int res = DifOst(a);
+Console.WriteLine(a);
+Console.WriteLine(res);
+*/
