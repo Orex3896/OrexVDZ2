dfd1316751a25b35db34d94759d8d335c9d17936
Vlad Oreshko <orex3896@gmail.com>
2022-12-09 00:13:05 +0300

    created task#2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,66 @@
     return(result);
 } 
 Console.WriteLine("Введите трехзначное число ");
-int a = Convert.ToInt32( Console.ReadLine() );
+int a = Convert.ToInt32(Console.ReadLine() );
 //int a = new Random().Next(100,999) ;
 int res = DifOst(a);
 Console.WriteLine(a);
 Console.WriteLine(res);
 */
+
+/*
+int Dif(int num)
+{
+    while(num>100)
+    {
+        num = num/10;
+        int res=num%100;
+        return(res);
+    }
+    
+ //   int A = num%1000;
+   // return A;
+}
+Console.WriteLine("Введите трехзначное число ");
+int b = Convert.ToInt32(Console.ReadLine() );
+int res = Dif(b);
+Console.WriteLine(res);
+
+*/
+
+int Diff(int num)//Не смог понять как вывести третью цифру если число <100
+{
+    /*if(num>0.001)
+    {
+        num=num/10;
+        
+        Console.WriteLine(num);
+    }
+    double ostmin=num%10;
+    return (ostmin);*/
+    
+    /*if(num<100)
+    {   
+        while(num<100){
+                      Console.WriteLine("третьей цифры нет");
+                      break;}
+    }*/
+    while(num>1000)
+    {
+        num=num/10;
+        
+        Console.WriteLine(num);
+    }
+    int ost= num%10; 
+    return(ost);
+    
+    
+}
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<100)
+{
+    Console.WriteLine("число меньше 100");
+}
+else {int result= Diff(n);
+Console.WriteLine($"из числа {n} третье число будет {result}");}
